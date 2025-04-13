7f6ee4504b2b004bb0d76f923a72381a22d17361
surypratapsingh <97216641+surypratapsingh@users.noreply.github.com>
2025-04-13 21:55:32 +0530

    Add files via upload

---
+++ tast2_assign3_module4.py
@@ -0,0 +1,9 @@
+import math
+n = float(input("Enter a number :"))
+sqrt_value = math.sqrt(n)
+log_value = math.log(n)
+sine_value = math.sin(n)
+
+print("Square Root :",sqrt_value)
+print("Logarithm :",log_value)
+print("Sine :",sine_value)
